a2b9965c38640f265eaddd292122710ee41eb363
Brad Vernon <ibspoof@gmail.com>
2015-08-21 13:25:53 -0700

    Adding show of how to pass info from command line

--- src/test/scala/simulations/basic/BasicExampleSimulation.scala
+++ src/test/scala/simulations/basic/BasicExampleSimulation.scala
@@ -7,11 +7,12 @@ import scala.concurrent.duration._
 
 class BasicExampleSimulation extends Simulation {
 
-  val scn = scenario("My Simple Scenario")
-    .exec(http("My favorite search engine")
-    .get("http://www.google.com"))
+    val scn = scenario("My Simple Scenario")
+      .exec(http("My favorite search engine")
+      .get("http://www.google.com"))
+
 
     setUp(scn
-      .inject(rampUsers(1) over 1.seconds)
+      .inject(rampUsers(Integer.getInteger("users", 1)) over 5.seconds)
     )
 }
